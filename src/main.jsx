bffaa762d5b5593f9eb707e9173caab147c2a8e4
daddyjanno <jn.drugmand@gmail.com>
2024-06-21 19:17:05 +0200

    main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import './styles.css'
+import Post from './pages/Post.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <Home />
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/post/:id" element={<Post />} />
+            </Routes>
+        </Router>
     </React.StrictMode>,
 )
