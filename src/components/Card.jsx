d2c14e72546e0a82ed1387a569aba52a857621a2
daddyjanno <jn.drugmand@gmail.com>
2024-06-21 19:16:50 +0200

    Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,20 @@
 const Card = (props) => {
+    const { id, title, img, desc, author, handlePostClick } = props
     return (
-        <div>
+        <div className="cursor-pointer" onClick={() => handlePostClick(id)}>
             <div className="max-w-sm rounded shadow-lg">
-                <img className="w-full" src={props.img} alt={props.title} />
+                <img className="w-full" src={img} alt={title} />
                 <div className="h-60 px-6 py-4">
                     <div className="mb-2 h-20 overflow-visible">
-                        <p className="text-xl font-bold">{props.title}</p>
+                        <p className="truncate text-xl font-bold">{title}</p>
                     </div>
                     <div>
-                        <p className="text-base text-gray-700">{props.desc}</p>
+                        <p className="text-base text-gray-700">{desc}</p>
                     </div>
                 </div>
                 <div className="px-6 pb-2 pt-4">
                     <span className="mb-2 mr-2 inline-block rounded-full bg-gray-200 px-3 py-1 text-sm font-semibold text-gray-700">
-                        {props.author}
+                        {author}
                     </span>
                 </div>
             </div>
