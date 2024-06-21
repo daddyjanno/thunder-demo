96e8a039a5d98b878c8d93abda06b40b132b2387
daddyjanno <jn.drugmand@gmail.com>
2024-06-21 18:31:53 +0200

    apiCalls

---
+++ src/utils/apiCalls.js
@@ -0,0 +1,29 @@
+const POSTS_URL = 'https://jsonplaceholder.typicode.com/posts?_limit=10'
+const USERS_URL = 'https://jsonplaceholder.typicode.com/users'
+
+export async function getPosts() {
+    try {
+        const response = await fetch(POSTS_URL)
+        if (!response.ok) {
+            throw new Error('Network response was not OK')
+        }
+        const posts = await response.json()
+        return posts
+    } catch (error) {
+        return { data: null, error: 'Erreur API' }
+    }
+}
+
+export async function getUsers() {
+    try {
+        const response = await fetch(USERS_URL)
+        if (!response.ok) {
+            throw new Error('Network response was not OK')
+        }
+        const users = await response.json()
+        console.log('users', users)
+        return users
+    } catch (error) {
+        return { data: null, error: 'Erreur API' }
+    }
+}
