0d07d1e5ccf25f27bd42d4f21d8aeb739fb4ac6b
daddyjanno <jn.drugmand@gmail.com>
2024-06-21 10:35:00 +0200

    Home component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import { TailwindIndicator } from '../utils/TailwindIndicator'
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="font-bold underline">Home</h1>
+            <TailwindIndicator />
+        </div>
+    )
+}
+
+export default Home
