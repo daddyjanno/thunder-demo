e93f7c99841f32d3a4b4266d31f66316198a79b5
daddyjanno <jn.drugmand@gmail.com>
2024-06-22 11:47:15 +0200

    main with header

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,17 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import './styles.css'
 import Post from './pages/Post.jsx'
+import Error from './pages/Error.jsx'
+import Header from './layout/Header.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <Router>
+            <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/post/:id" element={<Post />} />
+                <Route path="/posts/:id" element={<Post />} />
+                <Route path="*" element={<Error />} />
             </Routes>
         </Router>
     </React.StrictMode>,
