927ab3115c7e116e2b0b7c4cddfdf27dbf5bac33
daddyjanno <jn.drugmand@gmail.com>
2024-06-24 15:56:11 +0200

    Post page

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,12 +1,35 @@
 import { useParams } from 'react-router-dom'
+import { useFetch } from '../utils/hook/useFetch'
+import Loader from '../components/ui/Loader'
+import { useEffect, useState } from 'react'
+import Article from '../components/ui/Article'
 
-const Post = () => {
+export default function Post() {
     const { id } = useParams()
-    return (
-        <div>
-            <h1>Post - id:{id}</h1>
+    const { posts, users, isLoading, error } = useFetch()
+    const [post, setPost] = useState(null)
+    const [user, setUser] = useState(null)
+
+    useEffect(() => {
+        if (posts) {
+            const post = posts.find((post) => post.id === Number(id))
+            setPost(post)
+        }
+        if (users) {
+            const user = users.find((user) => user.id === post.userId)
+            setUser(user)
+        }
+    }, [posts, users])
+
+    return error ? (
+        <div className="flex h-screen items-center justify-center">
+            <span>{error}</span>
         </div>
+    ) : isLoading ? (
+        <Loader />
+    ) : posts && users ? (
+        <Article post={post} author={user} />
+    ) : (
+        <Loader />
     )
 }
-
-export default Post
