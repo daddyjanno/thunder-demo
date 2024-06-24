649bef806d1d7681250eb4bd3400f98f223356a4
daddyjanno <jn.drugmand@gmail.com>
2024-06-24 20:21:54 +0200

    Error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,10 +1,9 @@
 import { Link } from 'react-router-dom'
-import Home from './Home'
 
 const Error = () => {
     return (
-        <section className="bg-white">
-            <div className="mx-auto h-screen max-w-screen-xl content-center px-4 py-8 lg:px-6 lg:py-16">
+        <section>
+            <div className="mx-auto h-[81vh] max-w-screen-xl content-center px-4 py-8 lg:px-6 lg:py-16">
                 <div className="mx-auto max-w-screen-sm text-center">
                     <h1 className="text-primary-600 dark:text-primary-500 mb-4 text-7xl font-extrabold tracking-tight lg:text-9xl">
                         404
@@ -15,7 +14,7 @@ const Error = () => {
                     </p>
                     <Link
                         to={'/'}
-                        className="my-4 inline-flex rounded-lg bg-blue-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800"
+                        className="bg-ternary hover:bg-secondary my-4 inline-flex rounded-lg px-5 py-2.5 text-center text-sm font-medium text-white"
                     >
                         Retour à l'accueil
                     </Link>
