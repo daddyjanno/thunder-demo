836cfe9e235cc8fcb16c368ff37e5752f6ad89e2
daddyjanno <jn.drugmand@gmail.com>
2024-06-24 20:21:09 +0200

    Dropdown

--- src/components/ui/Dropdown.jsx
+++ src/components/ui/Dropdown.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { Button } from './Button'
 
 const Dropdown = ({ data, onSelectChange, value, label }) => {
     function handleChange(event) {
@@ -9,31 +9,34 @@ const Dropdown = ({ data, onSelectChange, value, label }) => {
     }
 
     return (
-        <div className="flex flex-row justify-center gap-2">
-            <label>{label}</label>
-            <div className="h-6">
-                <select
-                    className="h-full"
-                    value={value}
-                    onChange={handleChange}
-                >
-                    <option value={''}>---</option>
-                    {data.map((item) => {
-                        return (
-                            <option key={item.id} value={item.id}>
-                                {item.name}
-                            </option>
-                        )
-                    })}
-                </select>
-                <button
-                    onClick={resetDropdown}
-                    className="cursor-pointer bg-white px-2"
-                >
-                    x
-                </button>
+        <>
+            <div className="flex h-auto flex-col items-center justify-center gap-4 sm:flex-row">
+                <label className="dark:text-primary">{label}</label>
+                <div className="flex flex-row items-center justify-center">
+                    <select
+                        className="bg-ternary dark:bg-secondary dark:text-quaternary text-primary h-8 rounded-l-md px-2"
+                        value={value}
+                        onChange={handleChange}
+                    >
+                        <option value={''}>---</option>
+                        {data.map((item) => {
+                            return (
+                                <option key={item.id} value={item.id}>
+                                    {item.name}
+                                </option>
+                            )
+                        })}
+                    </select>
+                    <Button
+                        text={'reset'}
+                        className={
+                            'bg-ternary h-8 rounded-r-md p-1 px-2 font-thin'
+                        }
+                        onClick={resetDropdown}
+                    />
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
