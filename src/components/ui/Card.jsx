e31fd6844c900d66555a9353e932bd6ae9068dcc
daddyjanno <jn.drugmand@gmail.com>
2024-06-24 20:57:10 +0200

    Card

--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -11,8 +11,8 @@ const Card = (props) => {
                     src={`https://picsum.photos/400/200?random=${randomNumber}`}
                     alt="article picture"
                 />
-                <section className="dark:bg-quaternary rounded-xl px-6 py-4">
-                    <div className="dark:bg-ternary mb-2 h-20 w-full overflow-hidden p-2 drop-shadow-lg">
+                <section className="dark:bg-quaternary px-6 py-4">
+                    <div className="mb-2 h-20 w-full overflow-hidden p-2 drop-shadow-lg">
                         <h2 className="dark:text-primary line-clamp-2 break-words text-xl font-bold">
                             {capitalize(post.title)}
                         </h2>
