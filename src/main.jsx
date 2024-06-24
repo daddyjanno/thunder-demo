170230be69c083058f9faa6fe3d8d2324f6a8e19
daddyjanno <jn.drugmand@gmail.com>
2024-06-24 15:56:45 +0200

    main

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Home from './pages/Home.jsx'
-import './styles.css'
 import Post from './pages/Post.jsx'
 import Error from './pages/Error.jsx'
 import Header from './layout/Header.jsx'
+import Footer from './layout/Footer.jsx'
+import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
@@ -16,6 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
                 <Route path="/posts/:id" element={<Post />} />
                 <Route path="*" element={<Error />} />
             </Routes>
+            <Footer />
         </Router>
     </React.StrictMode>,
 )
